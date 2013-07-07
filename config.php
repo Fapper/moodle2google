d74fb2f316fbc80bc220ad3d06ec245fadbf07eb
Martin Bjeldbak Madsen <martin2madsen@gmail.com>
2013-07-07 22:42:38 +0200

    Move needed constants to config.php file

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+// All of these values can be found when browsing
+// the calendar export page at the link
+// <moodle root>/calendar/export.php
+//
+// I have provided some default ones THAT AREN'T VALID
+// merely as an example
+
+define('DOMAIN', 'http://sict.moodle.aau.dk'); // could also be https://huminfaau.moodle.aau.dk...
+define('USERNAME', 'mbma11@student.aau.dk'); // possibly an email, at least at Aalborg University
+define('AUTHTOKEN', 'e87d8abdd212e22cda3e53a687c5a6abdfd83db9'); // some hashed value like this
+
+?>
